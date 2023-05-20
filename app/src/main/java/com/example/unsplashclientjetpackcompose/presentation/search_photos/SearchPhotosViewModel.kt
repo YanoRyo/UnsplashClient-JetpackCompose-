832e96f378ec00eb585e3908f1e26cd5d6f1f6b2
YanoRyo <yanoryo7032@gmail.com>
2023-05-20 12:48:59 +0900

    SearchPhotosViewModelの作成

---
+++ app/src/main/java/com/example/unsplashclientjetpackcompose/presentation/search_photos/SearchPhotosViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.unsplashclientjetpackcompose.presentation.search_photos
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.unsplashclientjetpackcompose.common.NetworkResponse
+import com.example.unsplashclientjetpackcompose.domain.use_case.SearchPhotosUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchPhotosViewModel @Inject constructor(
+    private val searchPhotosUseCase: SearchPhotosUseCase,
+) : ViewModel() {
+    private val _state = mutableStateOf(SearchPhotosState())
+    val state: State<SearchPhotosState> = _state
+
+    init {
+        searchPhotos("baseball")
+    }
+
+    fun searchPhotos(query: String) {
+        searchPhotosUseCase(query).onEach { result ->
+            when (result) {
+                is NetworkResponse.Success -> {
+                    _state.value = SearchPhotosState(
+                        isLoading = false,
+                        photos = result.data ?: emptyList(),
+                    )
+                }
+                is NetworkResponse.Failure -> {
+                    _state.value = SearchPhotosState(error = result.error)
+                }
+                is NetworkResponse.Loading -> {
+                    _state.value = SearchPhotosState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
