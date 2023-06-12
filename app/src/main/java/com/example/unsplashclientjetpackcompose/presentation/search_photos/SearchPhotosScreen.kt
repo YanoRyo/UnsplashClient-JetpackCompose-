ec1691353d7312e93b97a789c218509780b8b9d7
YanoRyo <yanoryo7032@gmail.com>
2023-06-12 23:14:59 +0900

    SearchBarを画面に追加

--- app/src/main/java/com/example/unsplashclientjetpackcompose/presentation/search_photos/SearchPhotosScreen.kt
+++ app/src/main/java/com/example/unsplashclientjetpackcompose/presentation/search_photos/SearchPhotosScreen.kt
@@ -1,20 +1,33 @@
 package com.example.unsplashclientjetpackcompose.presentation.search_photos
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.unsplashclientjetpackcompose.presentation.search_photos.components.PhotoThumbnail
+import com.example.unsplashclientjetpackcompose.presentation.search_photos.components.SearchBar
 
 @Composable
 fun SearchPhotosScreen(
     viewModel: SearchPhotosViewModel = hiltViewModel(),
 ) {
     val state = viewModel.state.value
-    LazyColumn {
-        items(state.photos) {photo ->
-            PhotoThumbnail(photo = photo, onClick = {})
+    Scaffold(
+        topBar = {
+            SearchBar(searchText = "",
+                onDone = {  },
+                onSearchTextChanged = { },
+            )
+        }
+    ) {paddingValue ->
+        LazyColumn(modifier = Modifier.padding(paddingValue)) {
+            items(state.photos) {photo ->
+                PhotoThumbnail(photo = photo, onClick = {})
+            }
         }
     }
 }
